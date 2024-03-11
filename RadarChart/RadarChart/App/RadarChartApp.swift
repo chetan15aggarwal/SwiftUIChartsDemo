2eaecb784e8d9733c4f2c7af671174ec70e2cfc1
Chetan Aggarwal <chetan15aggarwal@gmail.com>
2024-03-11 21:16:47 +0100

    initial radar chart demo

---
+++ RadarChart/RadarChart/App/RadarChartApp.swift
@@ -0,0 +1,15 @@
+//
+// RadarChart
+// Created by Chetan Aggarwal.
+
+
+import SwiftUI
+
+@main
+struct RadarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RadarChart/RadarChart/Views/ContentView.swift
@@ -0,0 +1,22 @@
+//
+// RadarChart
+// Created by Chetan Aggarwal.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
