c9baaa65f7e752308c59d337e48f76a15f4eec3b
Chetan Aggarwal <chetan15aggarwal@gmail.com>
2024-03-11 22:00:55 +0100

    image for character added | top part of the view developed

--- RadarChart/RadarChart/Views/ContentView.swift
+++ RadarChart/RadarChart/Views/ContentView.swift
@@ -7,16 +7,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        IronmanStrengthView()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+struct IronmanStrengthView: View {
+    var body: some View {
+        ZStack {
+            Color.yellow.opacity(1).edgesIgnoringSafeArea(.all)
+            
+            ScrollView{
+                Text("IRON MAN")
+                    .font(.largeTitle)
+                    .foregroundStyle(.red)
+                    .italic()
+                    .bold()
+                
+                HStack {
+                    Image("ironman")
+                        .resizable()
+                        .frame(width: 150,height: 200)
+                    
+                    VStack{
+                        HStack{
+                            Text("Anthony Edward 'Tony' Stark")
+                                .font(.system(size: 25))
+                                .foregroundStyle(.red)
+                                .bold()
+                            Spacer()
+                        }
+                        
+                        Spacer()
+                            .frame(height: 10)
+                        
+                        HStack{
+                            Text("My armor, it was never a distraction or a hobby, it was a cocoon. And now, I'm a changed man. You can take away my house, all my tricks and toys. But one thing you can't take away… I am Iron Man. ―Tony Stark"
+                                 )
+                                .font(.system(size: 15))
+                                .foregroundStyle(.red).opacity(0.9)
+                                .bold()
+                            Spacer()
+                        }
+                        
+                        Spacer()
+                           
+                        Rectangle()
+                            .foregroundStyle(.red)
+                            .frame(height: 1)
+                        Spacer()
+                    }
+                    Spacer()
+                }
+                Spacer().frame(height: 50)
+            }
+            
+        }
+    }
 }
